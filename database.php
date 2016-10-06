a30e53e733cdc21dee9e2bbb5b3dfbdc648567e2
Cat Bendixen <catbendixen@gmail.com>
2016-10-06 16:24:42 +0200

    CRUD Database Project
    This is a school project for CPH Business Academy

---
+++ database.php
@@ -0,0 +1,37 @@
+<?php
+class Database
+{
+    private static $dbName = 'catbendixen_dk' ;
+    private static $dbHost = 'catbendixen.dk.mysql' ;
+    private static $dbUsername = 'catbendixen_dk';
+    private static $dbUserPassword = 'Zsgvafbp';
+     
+    private static $cont  = null;
+     
+    public function __construct() {
+        die('Init function is not allowed');
+    }
+     
+    public static function connect()
+    {
+       // One connection through whole application
+       if ( null == self::$cont )
+       {     
+        try
+        {
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); 
+        }
+        catch(PDOException $e)
+        {
+          die($e->getMessage()); 
+        }
+       }
+       return self::$cont;
+    }
+     
+    public static function disconnect()
+    {
+        self::$cont = null;
+    }
+}
+?>
