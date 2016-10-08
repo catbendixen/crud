9fa6a2cc1d89c382706aabf92a6c8e8ad6b83fc5
Catherine Bendixen <catbendixen@gmail.com>
2016-10-08 16:47:53 +0200

    Update project.php

--- project.php
+++ project.php
@@ -1,80 +1,70 @@
-<?php
-require 'database.php';
-$id = null;
-if ( !empty($_GET['Client_ID'])) {
-$id = $_REQUEST['Client_ID'];
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <?php
+     include 'database.php';
+    ?>
+    
+    <meta charset="utf-8">
+        <!--This project was created with the help of several tutorials--including, but not limited to Startutorial.com -->
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+   
+</head>
+ 
+<body class="background">
 
-if ( null==$id ) {
-header("Location: index.php");
-} else {
-$pdo = Database::connect();
-$sql = "SELECT * FROM project where Client_Client_ID = ?";
-$q = $pdo->prepare($sql);
-$q->execute(array($id));
-
-$data = $q->fetchAll();
-
-Database::disconnect();
-}
-
-?>
-
-
-
-<body>
-<div class="container">
-
-    <div class="span10 offset1">
+               <div class="container">
         <div class="row">
-            <h3>View the Projects</h3>
+            
+            
+            <h3>Data View from Multiple Tables</h3>
         </div>
-
-        <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-
-                    <th>Project</th>
-                    <th>Start Date</th>
-                    <th>End Date</th>
-                    <th>Details</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-        <tbody>
-
-        <?php
-        foreach($data as $key => $value){
-
-            echo '<tr>';
             
-            echo '<td>'. $value['Project_Name'] . '</td>';
-            echo '<td>'. $value['Project_Startdate'] . '</td>';
-            echo '<td>'. $value['Project_Enddate'] . '</td>'; 
-            echo '<td>'. $value['Project_Detail'] . '</td>';
-            
-            
-            echo '<td width=250>';
-            echo '<a class="btn btn-warning" href="read.php?Client_ID='.$data['Client_ID'].'">Read</a>';
-            echo ' ';
-            echo '<a class="btn btn-success" href="update.php?Client_ID='.$data['Client_ID'].'">Update</a>';
-            echo ' ';
-            echo '<a class="btn btn-danger" href="delete.php?Client_ID='.$data['Client_ID'].'">Delete</a>';
-            
-            echo '</td>';
-            echo '</tr>';
-
-}
-?>
-
-</tbody>
-</table>
-
-<div class="form-actions">
-<a class="btn" href="index.php">Back</a>
-</div>
-</div>
-</div> 
->
+        <div class="row">
+           
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Resource Name</th>
+                            <th>Project Name</th>
+                            <th>Project Start Date</th>
+                            <th>Client</th>
+                            <th>Hourly Usage Rate</th>
+                           
+                            
+                        </tr>
+                    </thead>
+                    
+                    <tbody>
+                        <?php
+                           
+                            $pdo = Database::connect();
+                            
+                            $sql= 'SELECT project.Project_Name, project.Project_Startdate, client.Client_Name, resources.Resources_Name, project_has_resources.hourly_Usage_Rate 
+                                    FROM project, client, resources, project_has_resources 
+                                    WHERE project.Client_Client_ID = client.Client_ID 
+                                    AND project.Project_ID = project_has_resources.Project_Project_ID 
+                                    AND project_has_resources.Resources_Resources_ID = resources.Resources_ID 
+                                    ORDER BY resources.Resources_Name LIMIT 0, 1000';
+                                foreach ($pdo->query($sql) as $row) {
+                                echo '<tr>';
+                                echo '<td>'. $row['Resources_Name'] . '</td>';
+                                echo '<td>'. $row['Project_Name'] . '</td>';
+                                echo '<td>'. $row['Project_Startdate'] . '</td>';
+                                echo '<td>'. $row['Client_Name'] . '</td>';
+                                echo '<td>'. $row['hourly_Usage_Rate'] . '</td>';
+                                
+                                echo '</tr>';
+                                }
+                        Database::disconnect();
+                        ?>
+                        </tbody>
+                </table>
+        </div>
+        <a class="btn" href="index.php">Back</a>
+    </div> <!-- /container -->
 </body>
 </html>
